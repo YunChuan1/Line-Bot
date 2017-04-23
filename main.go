35e6b5f4d0b5d37aca862bac38ddde475bea56b9
YunChuan1 <bravo102795@gmail.com>
2017-04-23 08:29:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -47,13 +47,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(text) { //type
+			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("韻全君"+":"+" 今天星期六 Made by 蔡侑憬")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(/*event.ReplyToken*/, linebot.NewTextMessage(message.ID+":"+" 今天星期六 Made by 蔡侑憬")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
 		}
 	}
 }
-//  message.Text  message.ID+
+//  message.Text  
