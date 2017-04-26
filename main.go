3addf81dcde0a68f17c00ef4195a1898b84bc1b1
YunChuan1 <bravo102795@gmail.com>
2017-04-26 21:34:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -192,7 +192,7 @@ Kan & Aki's CHANNEL: https://www.youtube.com/channel/UCNHqosTdwFPSK5OQsjFoS5g
 					pet = PetDB.GetNextPet()
 				}
 
-				out := fmt.Sprintf("您好，目前的動物：名為%s, 所在地為:%s, 敘述: %s 電話為:%s 圖片網址在: %s", pet.Name, pet.Resettlement, pet.Note, pet.Phone, pet.ImageName)
+				out := fmt.Sprintf("恩恩,然後咧??)
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(out)).Do(); err != nil {
 					log.Print(err)
 				}
