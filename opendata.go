7521470c39192d0ff71d1d67e85a9610a341fe9c
YunChuan1 <bravo102795@gmail.com>
2017-04-28 14:47:55 +0800

    Update opendata.go

--- opendata.go
+++ opendata.go
@@ -22,7 +22,7 @@ const (
 //TaipeiPets :Get from  http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c
 type TaipeiPets struct {
 	Result struct {
-		text-right display_none_print_show print_width  int    'json:"text-right display_none_print_show print_width"'
+		USA  int    `json:"text-right display_none_print_show print_width"`
 		Offset  int    `json:"offset"`
 		Limit   int    `json:"limit"`
 		Count   int    `json:"count"`
