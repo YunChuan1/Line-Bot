2061e132dfa574e7b5188f562f37b90a6ddd0136
YunChuan1 <bravo102795@gmail.com>
2017-04-28 14:31:36 +0800

    Update opendata.go

--- opendata.go
+++ opendata.go
@@ -14,7 +14,8 @@ package main
 
 // 「臺北市開放認養動物」API存取
 const (
-	OpenDataURL string = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c"
+	OpenDataURL string = "http://rate.bot.com.tw/xrt?Lang=zh-TW"
+	/*OpenDataURL string = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c"*/
 )
 
 
