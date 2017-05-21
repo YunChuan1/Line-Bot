623ab61541fd318852c25fe1256f734e751609ad
YunChuan1 <bravo102795@gmail.com>
2017-05-21 16:23:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -347,7 +347,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Print(err)
 				}*/
 			}
-			linebot.NewImagemapMessage{
+		}
+		linebot.NewImagemapMessage{
 				app.appBaseURL+"/static/rich",
 				"Imagemap alt text",
 				linebot.ImagemapBaseSize{1040, 1040},
@@ -356,7 +357,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				linebot.NewURIImagemapAction("https://store.line.me/family/play/en", linebot.ImagemapArea{0, 520, 520, 520}),
 				linebot.NewMessageImagemapAction("URANAI!", linebot.ImagemapArea{520, 520, 520, 520}),
 				}
-		}
+		
 	}
 }
 //  message.Text  
